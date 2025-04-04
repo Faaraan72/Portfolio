6cd7b613d96e7569211056d10731ac15b3f91d69
Faaraan72 <faaraan@wizar.io>
2025-04-04 20:26:42 +0530

    Slider Bug Fix

--- assets/js.js
+++ assets/js.js
@@ -16,16 +16,75 @@ document.querySelectorAll('.slider-controls li').forEach(function (indicator, in
     });
 });
 
-rightArrow.addEventListener('click', function () {
-    sectionIndex = (sectionIndex < numberOfSlide-1) ? sectionIndex + 1 : numberOfSlide-1;
-    document.querySelector('.slider-controls .selected').classList.remove('selected');
-    indicatorParents.children[sectionIndex].classList.add('selected');
-    slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
-});
+// rightArrow.addEventListener('click', function () {
+//     sectionIndex = (sectionIndex < numberOfSlide-1) ? sectionIndex + 1 : numberOfSlide-1;
+//     document.querySelector('.slider-controls .selected').classList.remove('selected');
+//     indicatorParents.children[sectionIndex].classList.add('selected');
+//     slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
+// });
 
-leftArrow.addEventListener('click', function () {
-    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+// leftArrow.addEventListener('click', function () {
+//     sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+//     document.querySelector('.slider-controls .selected').classList.remove('selected');
+//     indicatorParents.children[sectionIndex].classList.add('selected');
+//     slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
+// });
+
+// Add event listeners for both click and touch
+function addMultiEventListener(element, handler) {
+    element.addEventListener('click', handler);
+    element.addEventListener('touchend', function (e) {
+        e.preventDefault(); // Prevent double firing
+        handler();
+    }, { passive: false });
+}
+
+addMultiEventListener(rightArrow, function () {
+    if (sectionIndex < numberOfSlide - 1) {
+        sectionIndex++;
+        updateSlider();
+    }
+});
+addMultiEventListener(leftArrow, function () {
+    if (sectionIndex > 0) {
+        sectionIndex--;
+        updateSlider();
+    }
+});
+let touchStartX = 0;
+let touchEndX = 0;
+
+slider.addEventListener('touchstart', function (e) {
+    touchStartX = e.changedTouches[0].screenX;
+}, false);
+
+slider.addEventListener('touchend', function (e) {
+    touchEndX = e.changedTouches[0].screenX;
+    handleSwipeGesture();
+}, false);
+
+function handleSwipeGesture() {
+    const swipeThreshold = 50; // Minimum swipe distance in px
+
+    if (touchEndX < touchStartX - swipeThreshold) {
+        // Swiped Left
+        if (sectionIndex < numberOfSlide - 1) {
+            sectionIndex++;
+            updateSlider();
+        }
+    }
+
+    if (touchEndX > touchStartX + swipeThreshold) {
+        // Swiped Right
+        if (sectionIndex > 0) {
+            sectionIndex--;
+            updateSlider();
+        }
+    }
+}
+
+function updateSlider() {
     document.querySelector('.slider-controls .selected').classList.remove('selected');
     indicatorParents.children[sectionIndex].classList.add('selected');
-    slider.style.transform = 'translate(' + (sectionIndex) * -100/numberOfSlide +'%)';
-});
+    slider.style.transform = 'translate(' + (sectionIndex * -100 / numberOfSlide) + '%)';
+}
